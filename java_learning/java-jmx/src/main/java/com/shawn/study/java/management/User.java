52df33a8c46e3b8baa12b8a322e9714e536ce30b
mengzhibule <763462805ss@sina.com>
2021-07-25 17:40:24 +0800

    jmx

--- java_learning/java-jmx/src/main/java/com/shawn/study/java/management/User.java
+++ java_learning/java-jmx/src/main/java/com/shawn/study/java/management/User.java
@@ -1,2 +1,58 @@
-package com.shawn.study.java.management;public class User {
+package com.shawn.study.java.management;
+
+public class User {
+
+  private int id;
+  private String username;
+  private String password;
+  private String address;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  @Override
+  public String toString() {
+    return "User{"
+        + "id="
+        + id
+        + ", username='"
+        + username
+        + '\''
+        + ", password='"
+        + password
+        + '\''
+        + ", address='"
+        + address
+        + '\''
+        + '}';
+  }
 }
--- java_learning/java-jmx/src/main/java/com/shawn/study/java/management/UserManagerMBean.java
+++ java_learning/java-jmx/src/main/java/com/shawn/study/java/management/UserManagerMBean.java
@@ -1,2 +1,22 @@
-package com.shawn.study.java.management;public interface UserManagerMBean {
+package com.shawn.study.java.management;
+
+public interface UserManagerMBean {
+
+  int getId();
+
+  void setId(int id);
+
+  String getUsername();
+
+  void setUsername(String username);
+
+  String getPassword();
+
+  void setPassword(String password);
+
+  String getAddress();
+
+  void setAddress(String address);
+
+  String toString();
 }
