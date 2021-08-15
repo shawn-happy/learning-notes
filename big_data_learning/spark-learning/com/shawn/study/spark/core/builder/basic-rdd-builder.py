18a4cab402278fb23045636528239221f8b00ee9
mengzhibule <763462805ss@sina.com>
2021-08-16 01:22:17 +0800

    spark java learning

---
+++ big_data_learning/spark-learning/com/shawn/study/spark/core/builder/basic-rdd-builder.py
@@ -0,0 +1,27 @@
+from pyspark import SparkConf, SparkContext
+from tempfile import NamedTemporaryFile
+
+data = list(['Hello World', 'Hello Spark', 'Hello Hadoop', 'Spark Learning'])
+tempFile = NamedTemporaryFile(delete=True)
+tempFile.close()
+
+
+def build_in_memory_with_default_partition(spark_context: SparkContext):
+    rdd = spark_context.parallelize(data)
+    print(rdd.getNumPartitions())
+
+
+def build_in_memory_with_custom_partition(spark_context: SparkContext, number: int):
+    rdd = spark_context.parallelize(data, number)
+    print(rdd.getNumPartitions())
+
+
+if __name__ == '__main__':
+    conf = SparkConf().setMaster("local").setAppName('spark-learning')
+    sc = SparkContext(conf=conf)
+    print("default parallelism number :")
+    print(sc.defaultParallelism)
+    build_in_memory_with_default_partition(spark_context=sc)
+    build_in_memory_with_custom_partition(spark_context=sc, number=20)
+
+    sc.stop()
---
+++ big_data_learning/spark-scala-java-learning/src/main/java/com/shawn/study/big/data/spark/scala/WordCount.scala
@@ -0,0 +1,7 @@
+package com.shawn.study.big.data.spark.scala
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
