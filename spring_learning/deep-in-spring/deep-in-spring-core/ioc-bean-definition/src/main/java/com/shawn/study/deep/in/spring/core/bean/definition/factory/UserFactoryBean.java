e1d40978f252d8892a53c767100c5855eee309d5
shawn <gentlestshawn@gmail.com>
2022-05-24 01:13:49 +0800

    spring bean instantiation & init & destroy & gc demo

---
+++ spring_learning/deep-in-spring/deep-in-spring-core/ioc-bean-definition/src/main/java/com/shawn/study/deep/in/spring/core/bean/definition/factory/UserFactoryBean.java
@@ -0,0 +1,17 @@
+package com.shawn.study.deep.in.spring.core.bean.definition.factory;
+
+import com.shawn.study.deep.in.spring.core.ioc.domain.User;
+import org.springframework.beans.factory.FactoryBean;
+
+public class UserFactoryBean implements FactoryBean<User> {
+
+  @Override
+  public User getObject() throws Exception {
+    return User.getInstance();
+  }
+
+  @Override
+  public Class<?> getObjectType() {
+    return User.class;
+  }
+}
---
+++ spring_learning/deep-in-spring/deep-in-spring-core/ioc-bean-definition/src/test/java/com/shawn/study/deep/in/spring/core/bean/definition/BeanInstantiationTests.java
@@ -0,0 +1,66 @@
+package com.shawn.study.deep.in.spring.core.bean.definition;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.shawn.study.deep.in.spring.core.ioc.domain.User;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BeanInstantiationTests {
+
+  private static BeanInstantiationDemo instantiation;
+
+  @BeforeClass
+  public static void init() {
+    instantiation = new BeanInstantiationDemo();
+  }
+
+  @Test
+  public void testCreateBeanFromConstructor() {
+    User user = instantiation.createUserByConstructor();
+    assertNotNull(user);
+    assertEquals("shawn", user.getId());
+    assertEquals("shawn", user.getName());
+  }
+
+  @Test
+  public void testCreateBeanFromFactoryMethod() {
+    User user = instantiation.createUserByFactoryMethod();
+    assertNotNull(user);
+    assertEquals("1", user.getId());
+    assertEquals("Shawn", user.getName());
+  }
+
+  @Test
+  public void testCreateBeanFromBeanFactory() {
+    User user = instantiation.createUserByInstanceFactory();
+    assertNotNull(user);
+    assertEquals("1", user.getId());
+    assertEquals("Shawn", user.getName());
+  }
+
+  @Test
+  public void testCreateBeanFromFactoryBean() {
+    User user = instantiation.createUserByFactoryBean();
+    assertNotNull(user);
+    assertEquals("1", user.getId());
+    assertEquals("Shawn", user.getName());
+  }
+
+  @Test
+  public void testCreateBeanFromServiceLoader() {
+    User user = instantiation.createUserByServiceLoader();
+    assertNotNull(user);
+    assertEquals("1", user.getId());
+    assertEquals("Shawn", user.getName());
+  }
+
+  @Test
+  public void testCreateBeanFromAutowireCapableBeanFactory() {
+    User user = instantiation.createUserByAutowireCapableBeanFactory();
+    assertNotNull(user);
+    assertEquals("1", user.getId());
+    assertEquals("Shawn", user.getName());
+  }
+}
