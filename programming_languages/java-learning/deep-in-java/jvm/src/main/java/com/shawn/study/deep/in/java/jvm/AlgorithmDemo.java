71ce9f795ef9cfa69affbc2f658287ca47956743
shawn-happy <gentlestshawn@gmail.com>
2023-11-22 15:19:52 +0800

    jvm

---
+++ programming_languages/java-learning/deep-in-java/jvm/src/main/java/com/shawn/study/deep/in/java/jvm/AlgorithmDemo.java
@@ -0,0 +1,47 @@
+package com.shawn.study.deep.in.java.jvm;
+
+public class AlgorithmDemo {
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public double div(int a, int b) {
+    return (double) a / b;
+  }
+
+  public int multi(int a, int b) {
+    return a * b;
+  }
+
+  public int sub(int a, int b) {
+    return a - b;
+  }
+
+  public byte add(byte a, byte b) {
+    return (byte) (a + b);
+  }
+
+  public short add(short a, short b) {
+    return (short) (a + b);
+  }
+
+  public String concat(char a, char b) {
+    return String.valueOf(a).concat(String.valueOf(b));
+  }
+
+  public boolean merge(boolean a, boolean b) {
+    return a && b;
+  }
+
+  public float add(float a, float b) {
+    return a + b;
+  }
+
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  public long add(long a, long b) {
+    return a + b;
+  }
+}
