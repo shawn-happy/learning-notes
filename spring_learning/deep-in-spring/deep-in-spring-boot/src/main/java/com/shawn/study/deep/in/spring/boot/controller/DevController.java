50ad7f4f2b64e414a1f60064af53a35bde922019
shawn-happy <gentlestshawn@gmail.com>
2024-02-19 15:32:21 +0800

    paxos

--- spring_learning/deep-in-spring/deep-in-spring-boot/src/main/java/com/shawn/study/deep/in/spring/boot/controller/DevController.java
+++ spring_learning/deep-in-spring/deep-in-spring-boot/src/main/java/com/shawn/study/deep/in/spring/boot/controller/DevController.java
@@ -1,9 +1,11 @@
 package com.shawn.study.deep.in.spring.boot.controller;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +15,14 @@ public class DevController {
   @GetMapping("/echo/{message}")
   public String echo(@PathVariable String message) {
     return "dev: " + message;
+  }
+
+  @RequestMapping(
+      value = "/test",
+      method = {RequestMethod.GET, RequestMethod.POST},
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public User test(User user) {
+    return user;
   }
 }
