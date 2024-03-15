23913d284b2eeb18877b6490ac9873452a6ebefa
shawn-happy <gentlestshawn@gmail.com>
2024-03-15 17:01:24 +0800

    sort

--- ds_algo_learning/code/java/algorithms/src/main/java/io/github/shawn/algorithms/sort/Sorts.java
+++ ds_algo_learning/code/java/algorithms/src/main/java/io/github/shawn/algorithms/sort/Sorts.java
@@ -1,7 +1,11 @@
 package io.github.shawn.algorithms.sort;
 
 public class Sorts {
+
   public static int[] bubbleSort(int[] array) {
+    if (array == null || array.length <= 1) {
+      return array;
+    }
     int n = array.length;
     for (int i = 0; i < n - 1; i++) {
       boolean flag = false;
@@ -18,5 +22,66 @@ public class Sorts {
       }
     }
     return array;
+  }
+
+  public static void insertionSort(int[] array) {
+    if (array == null || array.length <= 1) {
+      return;
+    }
+    for (int i = 1; i < array.length; i++) {
+      int value = array[i];
+      int j = i - 1;
+      for (; j >= 0; j--) {
+        if (array[j] > value) {
+          array[j + 1] = array[j];
+        } else {
+          break;
+        }
+      }
+      array[j + 1] = value;
+    }
+  }
+
+  public static void selectionSort(int[] array) {
+    if (array == null || array.length <= 1) {
+      return;
+    }
+    int min;
+    for (int i = 0; i < array.length - 1; i++) {
+      min = i;
+
+      for (int j = array.length - 1; j > i; j--) {
+        if (array[j] < array[min]) {
+          min = j;
+        }
+      }
+      if (min != i) {
+        int tmp = array[i];
+        array[i] = array[min];
+        array[min] = tmp;
+      }
+    }
+  }
+
+  public static void shellSort(int[] arr) {
+    if (arr == null || arr.length <= 1) {
+      return;
+    }
+    int step = arr.length / 2;
+    while (step >= 1) {
+      for (int i = step; i < arr.length; i++) {
+        int value = arr[i];
+        int j = i - step;
+        for (; j >= 0; j -= step) {
+          if (value < arr[j]) {
+            arr[j + step] = arr[j];
+          } else {
+            break;
+          }
+        }
+        arr[j + step] = value;
+      }
+      step /= 2;
+    }
   }
 }
--- ds_algo_learning/code/java/algorithms/src/test/java/io/github/shawn/algorithms/sort/SortsTests.java
+++ ds_algo_learning/code/java/algorithms/src/test/java/io/github/shawn/algorithms/sort/SortsTests.java
@@ -5,16 +5,46 @@ import org.junit.Test;
 
 public class SortsTests {
 
-  private static final int[] array1 = {3, 5, 4, 1, 2, 6};
   private static final int[] expected1 = {1, 2, 3, 4, 5, 6};
-  private static final int[] array2 = {4, 23, -6, 78, 1, 54, 23, -6, -231, 9, 12};
   private static final int[] expected2 = {
     -231, -6, -6, 1, 4, 9, 12, 23, 23, 54, 78,
   };
 
   @Test
   public void testBubbleSort() {
+    final int[] array1 = {3, 5, 4, 1, 2, 6};
     Assert.assertArrayEquals(expected1, Sorts.bubbleSort(array1));
+    final int[] array2 = {4, 23, -6, 78, 1, 54, 23, -6, -231, 9, 12};
     Assert.assertArrayEquals(expected2, Sorts.bubbleSort(array2));
+  }
+
+  @Test
+  public void testInsertionSort() {
+    final int[] array1 = {3, 5, 4, 1, 2, 6};
+    Sorts.insertionSort(array1);
+    Assert.assertArrayEquals(expected1, array1);
+    final int[] array2 = {4, 23, -6, 78, 1, 54, 23, -6, -231, 9, 12};
+    Sorts.insertionSort(array2);
+    Assert.assertArrayEquals(expected2, array2);
+  }
+
+  @Test
+  public void testSelectionSort() {
+    final int[] array1 = {3, 5, 4, 1, 2, 6};
+    Sorts.selectionSort(array1);
+    Assert.assertArrayEquals(expected1, array1);
+    final int[] array2 = {4, 23, -6, 78, 1, 54, 23, -6, -231, 9, 12};
+    Sorts.selectionSort(array2);
+    Assert.assertArrayEquals(expected2, array2);
+  }
+
+  @Test
+  public void testShellSort() {
+    final int[] array1 = {3, 5, 4, 1, 2, 6};
+    Sorts.shellSort(array1);
+    Assert.assertArrayEquals(expected1, array1);
+    final int[] array2 = {4, 23, -6, 78, 1, 54, 23, -6, -231, 9, 12};
+    Sorts.shellSort(array2);
+    Assert.assertArrayEquals(expected2, array2);
   }
 }
